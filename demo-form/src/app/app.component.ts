6a7acd9a7eb99e0acae165d529c7a7faec9dab74
Morgan Guimard <morgan.guimard@docdoku.com>
2024-10-16 09:19:42 +0100

    demo forms

---
+++ demo-form/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'demo-form';
+  username = ""
+
+  log(f: NgForm) {
+    console.log(f)
+  }
+}
